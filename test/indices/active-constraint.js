f65df2876f7a927ae9d52f44033362c91f5bc9f8
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-12-16 18:38:13 +0000

    More capable test page

--- test/indices/active-constraint.js
+++ test/indices/active-constraint.js
@@ -1,29 +1,53 @@
+'use strict';
+
 require('imtables/shim');
-
+var $ = require('jquery');
 var imjs = require('imjs');
-
 var Options = require('imtables/options');
 var ActiveConstraint = require('imtables/views/active-constraint');
 
 var root = 'http://localhost:8080/intermine-demo';
-
-var flies = {
-  select: ['name', 'department.name'],
-  from: 'Employee',
-  where: [
-    ['name', '=', 'd*'],
-    ['age', '>', 35]
-  ]
-};
-
 var conn = imjs.Service.connect({root: root});
+var queries = [
+  {
+    name: 'attributes',
+    select: ['name', 'department.name', 'department.manager.name'],
+    from: 'Employee',
+    where: [
+      ['name', '=', 'd*'],
+      ['age', '>', 35]
+    ]
+  },
+  {
+    name: 'lists',
+    select: ['name', 'department.name', 'department.manager.name'],
+    from: 'Employee',
+    where: [
+      ['Employee', 'IN', 'My favourite employees']
+    ]
+  }
+]
 
-conn.query(flies)
-    .then(renderQuery)
-    .then(null, console.error.bind(console, 'Could not render query'));
+$(main);
 
-function renderQuery (query) {
+function main () {
   var container = document.querySelector('#demo');
+  queries.forEach(function (q) {
+    var div = document.createElement('div');
+    var h2 = document.createElement('h2');
+
+    container.appendChild(div);
+    h2.innerHTML = q.name
+    div.appendChild(h2);
+
+    conn.query(q)
+        .then(renderQuery.bind(null, div))
+        .then(null, console.error.bind(console, 'Could not render query'));
+  });
+
+}
+
+function renderQuery (container, query) {
   query.constraints.forEach(function (constraint) {
     var view = new ActiveConstraint({
       query: query,
