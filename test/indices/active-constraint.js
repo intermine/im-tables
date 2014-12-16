600ddbc7a7b85c2f4b6409a7efccf19301c83249
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-12-16 20:32:41 +0000

    Added helper so we can see the query state

--- test/indices/active-constraint.js
+++ test/indices/active-constraint.js
@@ -3,11 +3,38 @@
 require('imtables/shim');
 var $ = require('jquery');
 var imjs = require('imjs');
+var View = require('imtables/core-view');
 var Options = require('imtables/options');
 var ActiveConstraint = require('imtables/views/active-constraint');
 
 var root = 'http://localhost:8080/intermine-demo';
 var conn = imjs.Service.connect({root: root});
+
+var Counter = View.extend({
+
+  initialize: function (opts) {
+    View.prototype.initialize.apply(this, arguments);
+    this.query = opts.query;
+    this.model.set({count: 0});
+    this.listenTo(this.query, 'change:constraints', this.updateCount);
+    this.listenTo(this.model, 'change', this.render);
+    this.updateCount();
+  },
+
+  updateCount: function () {
+    var self = this;
+    this.query.count().then(function (c) {
+      self.model.set({count: c});
+    });
+  },
+
+  render: function () {
+    var name = this.query.name;
+    var count = this.model.get('count');
+    this.$el.empty().text(name + ' (' + count + ' rows)');
+  }
+});
+
 var queries = [
   {
     name: 'attributes',
@@ -23,7 +50,7 @@ var queries = [
     select: ['name', 'department.name', 'department.manager.name'],
     from: 'Employee',
     where: [
-      ['name', 'ONE OF', ['anne', 'brenda', 'carol']]
+      ['name', 'ONE OF', ['Anne', 'Brenda', 'Carol']]
     ]
   },
   {
@@ -57,13 +84,15 @@ function main () {
     div.appendChild(h2);
 
     conn.query(q)
-        .then(renderQuery.bind(null, div))
+        .then(renderQuery.bind(null, h2, div))
         .then(null, console.error.bind(console, 'Could not render query'));
   });
 
 }
 
-function renderQuery (container, query) {
+function renderQuery (heading, container, query) {
+  var counter = new Counter({el: heading, query: query});
+  counter.render();
   query.constraints.forEach(function (constraint) {
     var view = new ActiveConstraint({
       query: query,
